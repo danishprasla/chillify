81f087410ee2a2d99581e027f23cff2352d93fcf
danishprasla <danishprasla@hotmail.com>
2023-06-19 19:01:20 -0500

    css for player

--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -4,6 +4,7 @@ import LoginFormModal from '../LoginFormModal'
 import SignupFormModal from '../SignupFormModal'
 import * as sessionActions from "../../store/session";
 import { useDispatch } from 'react-redux';
+import './LandingPage.css'
 
 function LandingPage() {
 
@@ -14,25 +15,43 @@ function LandingPage() {
   }
 
   return (
-    <div>
-      <img className='landing-page-logo' src='https://cdn.discordapp.com/attachments/1117955975144538132/1118221613834444890/image.png'/>
-      <div>
-        <OpenModalButton
-          className='login-button'
-          buttonText="Log In"
-          modalComponent={<LoginFormModal/>}
-        />
+    <div className='landing-page-wrapper'>
+      <div className='landing-page-container'>
+        <div className='logo-wrapper'>
+          <img className='landing-page-logo' src='https://cdn.discordapp.com/attachments/1117955975144538132/1118221613834444890/image.png' />
+        </div>
+        <div className='landing-page-button-wrapper'>
+
+          <div className='login-button'>
+            <OpenModalButton
+              className='login-button'
+              buttonText="Log In"
+              modalComponent={<LoginFormModal />}
+            />
+          </div>
+          <div className='login-button'>
+            <OpenModalButton
+              className='login-button'
+              buttonText="Sign Up"
+              modalComponent={<SignupFormModal />}
+            />
+          </div>
+          <button className='login-button' onClick={demoLogin}>
+            Demo User Login
+          </button>
+        </div>
+
       </div>
-      <div>
-        <OpenModalButton
-          className='login-button'
-          buttonText="Sign Up"
-          modalComponent={<SignupFormModal/>}
-        />
+      <div className='landing-footer'>
+        <div className='footer-comp-1'>
+          <h2>
+            Danish Prasla
+          </h2>
+        </div>
+        <div className='footer-comp-2'>
+          
+        </div>
       </div>
-      <button onClick={demoLogin}>
-        Demo User Login
-      </button>
     </div>
   )
 }
