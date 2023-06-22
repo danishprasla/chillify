508c6d7812b7c68c6bcc26a9d60b3de4341dec8f
danishprasla <danishprasla@hotmail.com>
2023-06-22 16:56:33 -0500

    added tech icons

--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -40,16 +40,35 @@ function LandingPage() {
             Demo User Login
           </button>
         </div>
+        <h3 className='disclaimer'>
+          For Educational Purposes Only
+        </h3>
 
       </div>
       <div className='landing-footer'>
         <div className='footer-comp-1'>
-          <h2>
-            Danish Prasla
-          </h2>
+          <h4>
+            Made by Danish Prasla
+          </h4>
         </div>
-        <div className='footer-comp-2'>
-          
+        <div className='tech-icon-container'>
+
+          <img className='tech-icons' src="https://cdn.discordapp.com/attachments/1118303754714886259/1121552752133292133/JavaScript-logo.png" />
+
+          <img className='tech-icons' src="https://cdn.discordapp.com/attachments/1118303754714886259/1121556499269943367/1869px-Python-logo-notext.png" />
+
+          <img className='tech-icons' src="https://cdn.discordapp.com/attachments/1118303754714886259/1121551916028153888/2300px-React-icon.png" />
+
+          <img className='tech-icons' src="https://cdn.discordapp.com/attachments/1118303754714886259/1121552416844824636/redux.png" />
+
+          <img className='tech-icons' src="https://cdn.discordapp.com/attachments/1118303754714886259/1121553145743540284/ICAxwo4DA57SIvgIVTDpUP8EbeCT1qFmXQMAAAAASUVORK5CYII.png" />
+
+          <img className='tech-icons' src="https://cdn.discordapp.com/attachments/1118303754714886259/1121551721282408529/postgresql-icon.png" />
+
+          <img className='tech-icons' src="https://cdn.discordapp.com/attachments/1118303754714886259/1121556896625729686/free-html5-40-1175193.png" />
+          <img className='tech-icons' src="https://cdn.discordapp.com/attachments/1118303754714886259/1121557371194458202/css-512.png" />
+          <img className='tech-icons' src="https://cdn.discordapp.com/attachments/1118303754714886259/1121557732902834216/aws-icon-2048x2048-274bm1xi.png" />
+
         </div>
       </div>
     </div>
