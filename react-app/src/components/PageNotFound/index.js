117512685469991b88e65056c600a78e94dfa00e
danishprasla <danishprasla@hotmail.com>
2023-06-23 13:55:38 -0500

    added 404 page

---
+++ react-app/src/components/PageNotFound/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import './PageNotFound.css'
+
+function PageNotFound() {
+
+  return (
+    <div className="not-found">
+      <h1 className="lost-code"> 404:</h1>
+      <h3 className="lost-quote"> "...Not all those who wander are lost..."</h3>
+      <h3> - J. R. R. Tolkien</h3>
+      <h4> "...But you seem to be..."</h4>
+      <h4> - Danish Prasla </h4>
+      <img src="https://cdn.discordapp.com/attachments/1118303754714886259/1121872842737336425/iroh.gif" />
+      <h4>Click the home button on the side bar to be returned to paradise! Thank you for visiting!</h4>
+    </div>
+
+
+  )
+}
+
+export default PageNotFound
