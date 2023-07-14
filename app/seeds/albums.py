5d1bb3b224d753ee4458f963c58f78f28848f90b
danishprasla <danishprasla@hotmail.com>
2023-07-14 17:08:49 -0500

    added seeders

--- app/seeds/albums.py
+++ app/seeds/albums.py
@@ -10,6 +10,8 @@ def seed_albums():
     album4 = Album(author_id = 5, genre_id = 2, name= 'Dark Souls: Soundtrack', album_cover_photo='https://cdn.discordapp.com/attachments/1118303754714886259/1129129455521112265/ab67616d0000b273a9b9a9b870c117c9cfbb0f65.png')
     album5 = Album(author_id = 6, genre_id = 5, name= 'Doom: Soundtrack', album_cover_photo='https://cdn.discordapp.com/attachments/1118303754714886259/1129129856072958123/ab67616d0000b273aad36b64a1a78951b504bc4e.png')
     album6 = Album(author_id = 8, genre_id = 4, name= 'Lo-Fi Toons', album_cover_photo='https://cdn.discordapp.com/attachments/1118303754714886259/1129196638494269511/Lofitoonsart_320x.png')
+    album7 = Album(author_id = 12, genre_id = 4, name= 'Lo-Fi Girl Vol 1', album_cover_photo='https://cdn.discordapp.com/attachments/1118303754714886259/1129532637602648194/lofi-girl-lofi.gif')
+    album8 = Album(author_id = 12, genre_id = 4, name= 'Lo-Fi Girl Vol 2', album_cover_photo='https://cdn.discordapp.com/attachments/1118303754714886259/1129532909674565753/ab67616d0000b273ce6b0ef740f787932572ee5f.png')
     
     # album6 = Album(author_id = , genre_id = , name= '', album_cover_photo='')
 
@@ -19,6 +21,8 @@ def seed_albums():
     db.session.add(album4)
     db.session.add(album5)
     db.session.add(album6)
+    db.session.add(album7)
+    db.session.add(album8)
     db.session.commit()
 
 
